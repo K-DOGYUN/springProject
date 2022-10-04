7f040c72b2c97c042ad733457863d1ede7e85cab
com <com@DESKTOP-5JE210C>
2022-10-04 16:53:53 +0900

    aefaefaef

--- src/main/webapp/resources/sql/Customer.sql
+++ src/main/webapp/resources/sql/Customer.sql
@@ -1,18 +1,23 @@
 CREATE TABLE customer (
-    customer_id    VARCHAR2(100) PRIMARY KEY,
-    customer_pw    VARCHAR2(100) NOT NULL,
-    customer_name  VARCHAR2(100) NOT NULL,
-    customer_phone VARCHAR2(13) NOT NULL,
-    customer_state CHAR(1) DEFAULT 1,
-    reg_date       DATE DEFAULT sysdate,
-    update_date    DATE DEFAULT sysdate,
-    auth           VARCHAR2(8) DEFAULT 'customer',
-    business_number number(10)
+    customer_id      VARCHAR2(100) PRIMARY KEY,
+    customer_pw      VARCHAR2(100) NOT NULL,
+    customer_name    VARCHAR2(100) NOT NULL,
+    customer_phone   VARCHAR2(13) NOT NULL,
+    customer_state   NUMBER(1) DEFAULT 1,
+    reg_date         DATE DEFAULT sysdate,
+    update_date      DATE DEFAULT sysdate,
+    auth             VARCHAR2(8) DEFAULT 'customer',
+    business_number  NUMBER(10)
 );
 
-ALTER TABLE customer ADD business_number number(10);
+
+
+ALTER TABLE customer ADD business_number NUMBER(10);
 
 ALTER TABLE customer ADD auth VARCHAR2(8) DEFAULT 'customer';
+
+ALTER TABLE customer MODIFY
+    customer_state NUMBER(1) DEFAULT 1;
 
 SELECT
     *
@@ -35,18 +40,19 @@ SELECT
     addr_detail,
     addr_zipno,
     addr.customer_id,
-    addr.reg_date    AS addr_reg_date,
-    addr.update_date AS addr_update_date
+    addr.reg_date       AS addr_reg_date,
+    addr.update_date    AS addr_update_date
 FROM
-    customer c
-    LEFT OUTER JOIN address  addr ON c.customer_id = addr.customer_id
+    customer  c
+    LEFT OUTER JOIN address   addr ON c.customer_id = addr.customer_id
 WHERE
-    c.customer_id = 'aefdaf002@naver.com'
+    c.customer_id = 'zoxcibk@naver.com'
 ORDER BY
     addr_num ASC;
 
 SELECT
-    c.customer_id    customer_pw,
+    c.customer_id,
+    customer_pw,
     customer_name,
     customer_phone,
     customer_state,
@@ -58,21 +64,21 @@ SELECT
     addr_detail,
     addr_zipno,
     addr.customer_id,
-    addr.reg_date    AS addr_reg_date,
-    addr.update_date AS addr_update_date
+    addr.reg_date       AS addr_reg_date,
+    addr.update_date    AS addr_update_date
 FROM
-    customer c
-    LEFT OUTER JOIN address  addr ON c.customer_id = addr.customer_id
+    customer  c
+    LEFT OUTER JOIN address   addr ON c.customer_id = addr.customer_id
 WHERE
-    c.customer_id = 'aefdaf002@naver.com'
+    c.customer_id = 'customer002@naver.com'
 ORDER BY
     addr_num ASC;
 
 UPDATE customer
 SET
     customer_pw = '1',
-    customer_name= 'aaa',
-    customer_phone= '010-2222-2222',
+    customer_name = 'aaa',
+    customer_phone = '010-2222-2222',
     update_date = sysdate
 WHERE
     customer_id = 'aefawf@naver.com';
